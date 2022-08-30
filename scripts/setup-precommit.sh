9783a8ccb5e9c2498ea5742f774ec9cd8c5b822b
SOLUTION17god <110621010+SOLUTION17god@users.noreply.github.com>
2022-08-30 08:00:56 -0500

    Update setup-precommit.sh

--- scripts/setup-precommit.sh
+++ scripts/setup-precommit.sh
@@ -2,3 +2,18 @@
 
 cp scripts/pre-commit .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
+
+  "uptime" : 11000,
+  "procLaunch" : "2022-08-22 19:24:47.7886 -0500",
+  "procRole" : "unknown",
+  "version" : 2,
+  "userID" : 501,
+  "deployVersion" : 210,
+  "modelCode" : "iPhone13,3",
+  "procStartAbsTime" : 287257672706,
+  "coalitionID" : 486,
+  "osVersion" : {
+    "isEmbedded" : true,
+    "train" : "iPhone OS 15.6",
+    "releaseType" : "User",
+    "build" : "19G71"
